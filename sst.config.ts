32f6b20112e806d570e9264886ffeacb5954a6b1
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-06-16 16:33:49 -0700

    update: formatted

--- sst.config.ts
+++ sst.config.ts
@@ -2,27 +2,27 @@
 export default $config({
   app(input) {
     return {
-      name: "personal",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
+      name: 'personal',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      protect: ['production'].includes(input?.stage),
+      home: 'aws',
       providers: {
         aws: {
-          profile: "Personal",
+          profile: 'Personal'
         },
-        cloudflare: "6.3.0",
-      },
+        cloudflare: '6.3.0'
+      }
     };
   },
   async run() {
     let outputs = {};
-    const { readdirSync } = await import("fs");
-    for (const sst of readdirSync("./infra/")) {
-      const result = await import("./infra/" + sst);
+    const { readdirSync } = await import('fs');
+    for (const sst of readdirSync('./infra/')) {
+      const result = await import('./infra/' + sst);
       if (result.output) {
         Object.assign(outputs, result.output);
       }
     }
     return outputs;
-  },
+  }
 });
