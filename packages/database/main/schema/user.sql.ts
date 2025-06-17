33d32ac4954e0b68960ed3059d46d9817a3ea7d2
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-06-17 10:48:24 -0700

    update: no database

--- packages/database/main/schema/user.sql.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core';
-
-export const user = pgTable('user', {
-  id: uuid('id').notNull().primaryKey(),
-  email: text('email').notNull().unique()
-});
