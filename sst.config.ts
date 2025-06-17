4796ef89cc7be0988ad5db16e96edac433087189
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-06-16 22:41:38 -0700

    update: gh action aws profile conditional

--- sst.config.ts
+++ sst.config.ts
@@ -2,28 +2,28 @@
 export default $config({
   app(input) {
     return {
-      name: "personal",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
+      name: 'personal',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      protect: ['production'].includes(input?.stage),
+      home: 'aws',
       providers: {
         aws: {
-          profile: "Personal",
+          profile: process.env.GITHUB_ACTIONS ? undefined : 'Personal'
         },
-        cloudflare: "6.3.0",
-        github: "6.7.2",
-      },
+        cloudflare: '6.3.0',
+        github: '6.7.2'
+      }
     };
   },
   async run() {
     let outputs = {};
-    const { readdirSync } = await import("fs");
-    for (const sst of readdirSync("./infra/")) {
-      const result = await import("./infra/" + sst);
+    const { readdirSync } = await import('fs');
+    for (const sst of readdirSync('./infra/')) {
+      const result = await import('./infra/' + sst);
       if (result.output) {
         Object.assign(outputs, result.output);
       }
     }
     return outputs;
-  },
+  }
 });
