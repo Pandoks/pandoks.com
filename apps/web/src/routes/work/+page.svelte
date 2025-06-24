3ef11bc978b3808f2ce5d586fea0ec7012292bbc
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-06-24 13:01:15 -0700

    update: work

---
+++ apps/web/src/routes/work/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Badge } from '@pandoks.com/svelte/shadcn/badge';
+</script>
+
+<div class="-mt-4 flex flex-col items-center gap-4">
+  <Badge
+    class="border-neutral-300 bg-white/75 px-4 py-1 text-sm text-neutral-600"
+    variant="outline"
+  >
+    More to Come
+  </Badge>
+
+  <p class="font-garamond">
+    I currently strain solids and liquids at
+    <a href="https://www.sievedata.com" target="_blank" class="font-medium hover:underline">Sieve</a
+    >.
+  </p>
+</div>
