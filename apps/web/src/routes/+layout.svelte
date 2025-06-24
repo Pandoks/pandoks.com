7b09713b2dcd2ca75a056b8b7064b3dd6efc6460
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-06-24 09:54:56 -0700

    update: snippet nav

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
+  import { page } from '$app/state';
   import '../app.css';
 
   let { children } = $props();
 </script>
 
-<div>
-  <nav class="font-garamond flex flex-col">
-    <a data-sveltekit-preload-code href="/">Jason Kwok</a>
-    <a data-sveltekit-preload-code href="/blog">Blog</a>
-  </nav>
+<nav
+  class="font-inter fixed top-1/2 flex -translate-y-9/12 flex-col space-y-3 text-sm sm:pl-3 md:pl-7"
+>
+  {@render navLink('/', 'Jason Kwok')}
+  {@render navLink('/socials', 'Socials')}
+  {@render navLink('/work', 'Work')}
+  {@render navLink('/blog', 'Blog')}
+</nav>
+
+<div
+  class="flex min-h-screen items-center pr-[clamp(1rem,1vw,5rem)] pl-[clamp(6rem,20vw,13rem)] lg:justify-center lg:px-0"
+>
   {@render children()}
 </div>
+
+{#snippet navLink(href: string, text: string)}
+  <a
+    data-sveltekit-preload-code
+    {href}
+    class={page.route.id === href ? 'underline' : 'hover:underline'}>{text}</a
+  >
+{/snippet}
