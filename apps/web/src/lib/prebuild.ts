4c4138b7b44e1383c2f648e36b8a42ab797001ff
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-07-01 18:25:49 -0700

    update: more efficient image downloader

--- apps/web/src/lib/prebuild.ts
+++ apps/web/src/lib/prebuild.ts
@@ -1,21 +1,41 @@
 import { NOTION_DATABASE_ID } from '$env/static/private';
-import { getPageAndBlocksData } from './notion';
+import { notion } from './notion';
 import { downloadSignedUrlImage } from './utils';
 
 export const downloadBlogImages = async () => {
-  const posts = await getPageAndBlocksData({
-    databaseId: NOTION_DATABASE_ID,
-    filter: {
-      property: 'Publish',
-      checkbox: {
-        equals: true
-      }
-    }
-  });
+  let allPublishedPages = [];
+  let pageCursor;
+  do {
+    const pagesResponse = await notion.databases.query({
+      database_id: NOTION_DATABASE_ID,
+      filter: {
+        property: 'Publish',
+        checkbox: {
+          equals: true
+        }
+      },
+      sorts: [{ timestamp: 'created_time', direction: 'descending' }],
+      start_cursor: pageCursor
+    });
+
+    allPublishedPages.push(...pagesResponse.results);
+    pageCursor = pagesResponse.next_cursor;
+  } while (pageCursor);
+
   let imageDownloads = [];
-  for (const post of posts) {
-    for (const block of post.blocks) {
-      if (block.type === 'image') {
+  for (const page of allPublishedPages) {
+    let pageCursor;
+    do {
+      const blockResponse = await notion.blocks.children.list({
+        block_id: page.id,
+        start_cursor: pageCursor,
+        page_size: 100
+      });
+      const imageBlocks = blockResponse.results.filter(
+        (block) => !block.archived && !block.in_trash && block.type === 'image'
+      );
+
+      for (const block of imageBlocks) {
         imageDownloads.push(
           downloadSignedUrlImage({
             url: block[block.type].file.url,
@@ -24,8 +44,11 @@ export const downloadBlogImages = async () => {
           })
         );
       }
-    }
+
+      pageCursor = blockResponse.next_cursor;
+    } while (pageCursor);
   }
+
   await Promise.all(imageDownloads);
 };
 
