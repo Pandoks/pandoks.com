e67bca4fe8b0ebcd0834d9815bdf760844c6af77
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-07-03 22:09:42 -0700

    update: moved to top left nav

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+  import { page } from '$app/state';
   import '../app.css';
 
   let { children } = $props();
+  const pageSlug = $derived(new URL(page.url).pathname);
 </script>
 
-<div class="flex h-dvh items-center justify-center">
+<nav class="font-inter bg-background fixed flex gap-2 rounded-br-xs p-2 text-sm">
+  {@render navLink('/', 'Jason Kwok')}
+  {@render navLink('/socials', 'Socials')}
+  {@render navLink('/work', 'Work')}
+  {@render navLink('/blog', 'Blog')}
+</nav>
+
+<div class="flex min-h-dvh items-center justify-center">
   {@render children()}
 </div>
+
+{#snippet navLink(href: string, text: string)}
+  <a
+    data-sveltekit-preload-code="eager"
+    {href}
+    class={pageSlug === href ? 'underline' : 'hover:underline'}>{text}</a
+  >
+{/snippet}
