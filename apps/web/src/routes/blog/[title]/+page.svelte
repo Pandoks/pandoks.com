7b25f96b129ad26b18ce5490a52a14ec22725c77
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-07-05 11:51:11 -0700

    update: generous spacing

--- apps/web/src/routes/blog/[title]/+page.svelte
+++ apps/web/src/routes/blog/[title]/+page.svelte
@@ -7,7 +7,7 @@
 
 <div class="font-garamond mx-3 sm:max-w-[75vw] xl:max-w-[60vw] 2xl:max-w-[clamp(0rem,60vw,75rem)]">
   <h1 class="text-xl font-black">{data.title}</h1>
-  <p class="mb-3 text-sm">{data.createdTime}</p>
+  <p class="mb-5 text-sm">{data.createdTime}</p>
 
   {#each data.blocks as block}
     {@render blockRender(block)}
@@ -22,7 +22,7 @@
   {:else if block.type === 'heading_3'}
     <h4 class="font-semibold">{block.text}</h4>
   {:else if block.type === 'paragraph'}
-    <p class="mb-3">{block.text}</p>
+    <p class="mb-5">{block.text}</p>
   {:else if block.type === 'link'}
     <div class="underline decoration-dashed hover:cursor-pointer hover:decoration-solid">
       <a href={block.href} target="_blank">{block.text}</a>
@@ -31,14 +31,14 @@
     <br />
   {:else if block.type === 'image'}
     {#if dev}
-      <img class="mx-auto mb-3 rounded-xs" src={block.url} alt="A thousand words" />
+      <img class="mx-auto mb-5 rounded-xs" src={block.url} alt="A thousand words" />
     {:else}
-      <enhanced:img class="mx-auto mb-3 rounded-xs" src={block.url} alt="A thousand words" />
+      <enhanced:img class="mx-auto mb-5 rounded-xs" src={block.url} alt="A thousand words" />
     {/if}
   {:else if block.type === 'code'}
     <!-- NOTE: this NEEDS to be formatted weirdly because of the pre tag -->
     <pre
-      class="bg-highlight scrollbar-thin mb-3 overflow-x-auto rounded-xs p-4"
+      class="bg-highlight scrollbar-thin mb-5 overflow-x-auto rounded-xs p-4"
       style="scrollbar-width: thin;"><code class="font-mono text-sm"
         >{@html hljs.highlight(block.code, { language: block.language }).value}</code
       ></pre>
