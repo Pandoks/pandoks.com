2aeeaa59c15246a133de7272e2a0a7fb18520da1
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-07-06 14:17:10 -0700

    update: Tzugi

--- apps/web/src/routes/work/+page.svelte
+++ apps/web/src/routes/work/+page.svelte
@@ -3,19 +3,18 @@
 </script>
 
 <div class="flex flex-col items-center gap-4">
-  <Badge
-    class="border-neutral-300 bg-white/75 px-4 py-1 text-sm text-neutral-600"
-    variant="outline"
-  >
-    More to Come
-  </Badge>
-
   <ul class="font-garamond list-inside list-disc text-lg">
     <li>
       I currently strain solids and liquids at
       <a href="https://www.sievedata.com" target="_blank" class="font-medium hover:underline">
         Sieve
       </a>.
+    </li>
+    <li>
+      Personally working on
+      <span class="font-medium hover:cursor-pointer hover:underline hover:decoration-dashed">
+        Tzugi
+      </span>.
     </li>
   </ul>
 </div>
