3cb055d3ccfa7d00091c1b306f8cc257ee0a313e
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-07-16 17:36:27 -0700

    update: added web only for desktop

--- infra/browser.ts
+++ infra/browser.ts
@@ -5,3 +5,11 @@ export const desktopDev = new sst.x.DevCommand('DesktopDev', {
     directory: 'apps/desktop'
   }
 });
+
+export const desktopWebDev = new sst.x.DevCommand('DesktopWebDev', {
+  dev: {
+    autostart: false,
+    command: 'pnpm run dev',
+    directory: 'apps/web'
+  }
+});
