54f9017e2d05eda79f61c376ffb855f3e0e55a9a
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-07-24 11:08:05 -0700

    update: hover radius

--- apps/desktop/src/routes/+page.svelte
+++ apps/desktop/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="fixed top-0 left-1/2 -translate-x-1/2 items-center bg-amber-200 px-10 pb-10"
+  class="fixed top-0 left-1/2 -translate-x-1/2 items-center bg-amber-200 px-5 pb-3"
   role="presentation"
   onmouseenter={() => (proximityHover = true)}
   onmouseleave={() => ((proximityHover = false), (showFull = false))}
