8d4bba8b7456cbb58ff3e9478069d0e545102755
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-08-18 14:36:15 -0700

    update: send texts based off of person

--- apps/functions/src/api/todo.ts
+++ apps/functions/src/api/todo.ts
@@ -1,15 +1,28 @@
-import { PublishCommand, PublishCommandOutput, SNS } from '@aws-sdk/client-sns';
 import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import type { PageObjectResponse } from '@notionhq/client';
 import { Resource } from 'sst';
+import twilio from 'twilio';
+import { MessageInstance } from 'twilio/lib/rest/api/v2010/account/message';
 
-const sns = new SNS({ region: 'us-west-1' });
-const phoneNumbers = {
+const NAME_PROPERTY_KEYS = ['Assigned To', 'Person', 'Buyer', 'Assignee'];
+
+const PHONE_NUMBERS = {
   'Manda Wong': Resource.MichellePhoneNumber.value,
+  'BLAINE Manda Wong': Resource.MichellePhoneNumber.value,
   Pandoks: Resource.KwokPhoneNumber.value
 };
 
+const twilioClient = twilio(Resource.TwilioAccountSid.value, Resource.TwilioAuthToken.value);
+
+/**
+ * Request Requirements:
+ *  - Method: POST
+ *  - Headers:
+ *    - auth: NOTION_TODO_REMIND_AUTH
+ *    - people?: person1,person2,person3
+ *    - message?: message
+ */
 export const textTodoHandler = async (event: APIGatewayProxyEventV2) => {
-  console.log('event:', event);
   if (event.requestContext.http.method !== 'POST') {
     return new Response('Method Not Allowed', { status: 405 });
   }
@@ -18,21 +31,54 @@ export const textTodoHandler = async (event: APIGatewayProxyEventV2) => {
     return new Response('Unauthorized', { status: 401 });
   }
 
-  const assignedTo = JSON.parse(event.body!).data.properties['Assigned To'].people.map(
-    (person) => person.name
-  );
-  console.log('Assigned To:', assignedTo);
+  let phoneNumbers: string[] = [];
+
+  if (event.headers.people) {
+    const people = event.headers.people.split(',');
+    for (const person of people) {
+      if (PHONE_NUMBERS.hasOwnProperty(person)) {
+        phoneNumbers.push(PHONE_NUMBERS[person]);
+      }
+    }
+  }
+
+  const responseBody: NotionWebhookBody = JSON.parse(event.body!);
+  const properties = responseBody.data.properties;
+  let people: Person[] = [];
+  for (const key of NAME_PROPERTY_KEYS) {
+    if (properties.hasOwnProperty(key)) {
+      people.push(...(properties[key] as PersonProperty).people);
+    }
+  }
+  for (const person of people) {
+    if (
+      PHONE_NUMBERS.hasOwnProperty(person.name) &&
+      !phoneNumbers.includes(PHONE_NUMBERS[person.name])
+    ) {
+      phoneNumbers.push(PHONE_NUMBERS[person.name]);
+    }
+  }
+
+  if (!phoneNumbers.length) {
+    return new Response('OK', { status: 200 });
+  }
+
   try {
-    let smsMessages: Promise<PublishCommandOutput>[] = [];
-    for (const person of assignedTo) {
-      console.log('Sending SMS to', person, 'at', phoneNumbers[person]);
-      smsMessages.push(
-        sns.send(new PublishCommand({ Message: 'hi', PhoneNumber: phoneNumbers[person] }))
+    let texts: Promise<MessageInstance>[] = [];
+    for (const phoneNumber of phoneNumbers) {
+      texts.push(
+        twilioClient.messages.create({
+          body: event.headers.message || '📝 Todo Reminder',
+          from: Resource.TwilioPhoneNumber.value,
+          to: phoneNumber
+        })
       );
     }
-    const smsResponses = await Promise.all(smsMessages);
-    for (const smsResponse of smsResponses) {
-      console.log('SMS Response:', smsResponse);
+    const settled = await Promise.allSettled(texts);
+    for (const settledText of settled) {
+      if (settledText.status === 'rejected') {
+        console.error('ERROR:', settledText.reason);
+      }
     }
 
     return new Response('OK', { status: 200 });
@@ -40,4 +86,37 @@ export const textTodoHandler = async (event: APIGatewayProxyEventV2) => {
     console.error('ERROR:', e);
     return new Response('Internal Server Error', { status: 500 });
   }
+};
+
+/** ========== TYPES ========== */
+type NotionAutomationSource = {
+  type: 'automation';
+  automation_id: string;
+  action_id: string;
+  event_id: string;
+  user_id: string;
+  attempt: number;
+};
+
+type NotionWebhookBody = {
+  source: NotionAutomationSource;
+  data: PageObjectResponse;
+};
+
+type PersonProperty = {
+  id: string;
+  type: string;
+  number: number | null;
+  people: Person[];
+};
+
+type Person = {
+  object: 'string';
+  id: string;
+  name: string;
+  avatar_url: string;
+  type: 'person';
+  person: {
+    email: string;
+  };
 };
