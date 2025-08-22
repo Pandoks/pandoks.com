ebf34f0433ef2d4a697d42b7c6956f924f53d9d4
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-08-22 12:59:10 -0700

    update: cloudflare ips

--- infra/dns.ts
+++ infra/dns.ts
@@ -1,1 +1,7 @@
 export const domain = $app.stage === 'production' ? 'pandoks.com' : 'dev.pandoks.com';
+
+const cloudflareIpRequest = await fetch('https://api.cloudflare.com/client/v4/ips');
+export const cloudflareIps: {
+  result: { ipv4_cidrs: string[]; ipv6_cidrs: string[]; etag: string };
+  success: boolean;
+} = await cloudflareIpRequest.json();
