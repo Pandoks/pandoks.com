f96adf95910c990b49210ff8d6e288c0cbfa3059
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-08-25 09:07:39 -0700

    add: planetscale

--- sst.config.ts
+++ sst.config.ts
@@ -12,7 +12,8 @@ export default $config({
         },
         cloudflare: '6.3.0',
         github: '6.7.2',
-        hcloud: { token: process.env.HCLOUD_TOKEN, version: '1.24.0' }
+        hcloud: { token: process.env.HCLOUD_TOKEN, version: '1.24.0' },
+        planetscale: '0.4.1'
       }
     };
   },
