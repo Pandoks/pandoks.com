15f7a808c4056fe5b5537a3b90f62a3660229577
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-08-30 23:05:07 -0700

    update: example domain

--- infra/dns.ts
+++ infra/dns.ts
@@ -1,3 +1,6 @@
+import { secrets } from './secrets';
+import { publicLoadBalancer } from './vps/vps';
+
 export const domain = $app.stage === 'production' ? 'pandoks.com' : 'dev.pandoks.com';
 
 const cloudflareIpRequest = await fetch('https://api.cloudflare.com/client/v4/ips');
@@ -5,3 +8,25 @@ export const cloudflareIps: {
   result: { ipv4_cidrs: string[]; ipv6_cidrs: string[]; etag: string };
   success: boolean;
 } = await cloudflareIpRequest.json();
+
+if (publicLoadBalancer) {
+  const EXAMPLE_DOMAIN = 'example.pandoks.com';
+  new cloudflare.DnsRecord('TestDomainIpv4', {
+    name: EXAMPLE_DOMAIN,
+    zoneId: secrets.cloudflare.ZoneId.value,
+    type: 'A',
+    proxied: true,
+    ttl: 1,
+    comment: 'hetzner k3s',
+    content: publicLoadBalancer.ipv4
+  });
+  new cloudflare.DnsRecord('TestDomainIpv6', {
+    name: EXAMPLE_DOMAIN,
+    zoneId: secrets.cloudflare.ZoneId.value,
+    type: 'AAAA',
+    proxied: true,
+    ttl: 1,
+    comment: 'hetzner k3s',
+    content: publicLoadBalancer.ipv6
+  });
+}
