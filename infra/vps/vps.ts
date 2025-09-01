b8d86a1b37fb6d68f48981702e5be3c6648234df
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-09-01 01:22:01 -0700

    update: no servers

--- infra/vps/vps.ts
+++ infra/vps/vps.ts
@@ -86,9 +86,9 @@ if (needToSetCertificateSecret) {
 }
 
 // NOTE: if you want to downsize the cluster, remember to manually drain remove the nodes with `kubectl drain` & `kubectl delete node`
-const CONTROL_PLANE_NODE_COUNT = $app.stage === 'production' ? 0 : 3;
+const CONTROL_PLANE_NODE_COUNT = $app.stage === 'production' ? 0 : 0;
 const CONTROL_PLANE_HOST_START_OCTET = 10;
-const WORKER_NODE_COUNT = $app.stage === 'production' ? 0 : 3;
+const WORKER_NODE_COUNT = $app.stage === 'production' ? 0 : 0;
 const WORKER_HOST_START_OCTET = 20;
 // NOTE: servers can only be upgraded, not downgraded because disk size needs to be >= than the previous type
 const SERVER_TYPE = $app.stage === 'production' ? 'ccx13' : 'cpx11';
