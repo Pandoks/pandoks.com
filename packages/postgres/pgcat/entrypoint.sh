ab308b7bf3e2cca341d14435af38dec390aadbf7
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-09-03 16:23:41 -0700

    update: check for envs

--- packages/postgres/pgcat/entrypoint.sh
+++ packages/postgres/pgcat/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+set -eu
+
+for v in \
+  POSTGRES_USER \
+  POSTGRES_PASSWORD \
+  POSTGRES_DB \
+  PGPOOLPASS \
+  MASTER_HOST_1 \
+  SLAVE_HOST1_1; do
+  eval ": \${$v:?Missing $v}"
+done
 
 envsubst </tmp/conf_templates/pgcat.toml >/etc/pgcat/pgcat.toml
 
