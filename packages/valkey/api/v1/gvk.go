49af55616b0f49c1f7517250a4ab5eec8a23164a
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-09-22 11:08:27 -0700

    update: apiVersion: valkey/v1

--- packages/valkey/api/v1/gvk.go
+++ packages/valkey/api/v1/gvk.go
@@ -2,11 +2,11 @@ package v1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	crscheme "sigs.k8s.io/controller-runtime/pkg/scheme"
+	controllerRuntimeScheme "sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
-var GroupVersion = schema.GroupVersion{Group: "valkey.pandoks.dev", Version: "v1"}
+var GroupVersion = schema.GroupVersion{Group: "valkey", Version: "v1"}
 
-var SchemeBuilder = &crscheme.Builder{GroupVersion: GroupVersion}
+var SchemeBuilder = &controllerRuntimeScheme.Builder{GroupVersion: GroupVersion}
 
 var AddToScheme = SchemeBuilder.AddToScheme
