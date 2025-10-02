60ed80291b17693d267e81ead494280eab4ab832
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-10-02 13:41:19 -0700

    update: client connections

--- packages/valkey/internal/controller/cluster.go
+++ packages/valkey/internal/controller/cluster.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	valkeyv1 "valkey/operator/api/v1"
 
+	"github.com/valkey-io/valkey-go"
 	appsv1 "k8s.io/api/apps/v1"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -12,15 +13,37 @@ import (
 func (r *ValkeyClusterReconciler) reconcileClusterStatefulSet(ctx context.Context, valkeyCluster *valkeyv1.ValkeyCluster, statefulSet *appsv1.StatefulSet) error {
 	logger := log.FromContext(ctx)
 
+	// fqdn: fully qualified domain name
 	var podFQDNs []string
 	for i := range *statefulSet.Spec.Replicas {
-		fqdn := fmt.Sprintf("%s-%d.%s.%s.svc.cluster.local:6379",
+		fqdn := fmt.Sprintf("%s-%d.%s.%s.svc.cluster.local:%d",
 			statefulSet.Name,
 			i,
-			headlessServiceName(valkeyCluster),
+			valkeyCluster.HeadlessServiceName(),
 			valkeyCluster.Namespace,
+			ValkeyClientPort,
 		)
 		podFQDNs = append(podFQDNs, fqdn)
+	}
+
+	var clients []valkey.Client
+	for _, fqdn := range podFQDNs {
+		client, err := valkey.NewClient(valkey.ClientOption{InitAddress: []string{fqdn}})
+		if err != nil {
+			for _, c := range clients {
+				c.Close()
+			}
+			return err
+		}
+
+		if err = client.Do(ctx, client.B().Ping().Build()).Error(); err != nil {
+			client.Close()
+			for _, c := range clients {
+				c.Close()
+			}
+			return err
+		}
+		clients = append(clients, client)
 	}
 
 	return nil
