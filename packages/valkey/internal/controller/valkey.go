419e834f400d022461ddc0c36baa1d7e2458233c
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-10-04 19:10:25 -0700

    update: parse cluster nodes into go struct

--- packages/valkey/internal/controller/valkey.go
+++ packages/valkey/internal/controller/valkey.go
@@ -3,6 +3,9 @@ package controller
 import (
 	"context"
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/valkey-io/valkey-go"
 )
@@ -43,4 +46,80 @@ func (r *ValkeyClusterReconciler) queryClusterNodes(ctx context.Context, client 
 	}
 
 	return output, nil
+}
+
+func (r *ValkeyClusterReconciler) parseClusterNodes(clusterNodeOutput string, fqdnMap map[string]string) (*ClusterTopology, error) {
+	topology := &ClusterTopology{
+		Nodes: map[string]*ClusterNode{},
+	}
+
+	lines := slices.Collect(strings.SplitSeq(strings.TrimSpace(clusterNodeOutput), "\n"))
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		fields := strings.Fields(line)
+		if len(fields) < 8 {
+			continue
+		}
+
+		clusterNode := &ClusterNode{
+			ID:        fields[0],
+			Connected: fields[7] == "connected",
+		}
+
+		hostPort := strings.Split(fields[1], "@")[0]
+		parts := strings.Split(hostPort, ":")
+		if len(parts) == 2 {
+			host := parts[0]
+			port, _ := strconv.Atoi(parts[1])
+
+			clusterNode.Host = host
+			clusterNode.Port = port
+			clusterNode.FQDN = fqdnMap[host]
+		}
+
+		flags := slices.Collect(strings.SplitSeq(fields[2], ","))
+		for _, flag := range flags {
+			switch flag {
+			case "master":
+				clusterNode.Role = NodeRoleMaster
+			case "slave", "replica":
+				clusterNode.Role = NodeRoleSlave
+				clusterNode.MasterID = fields[3]
+			}
+		}
+
+		if clusterNode.Role == NodeRoleMaster {
+			const slotRangeStartIndex = 8
+			for i := slotRangeStartIndex; i < len(fields); i++ {
+				stringSlotRange := fields[i]
+				if strings.HasPrefix(stringSlotRange, "[") && strings.HasSuffix(stringSlotRange, "]") {
+					continue
+				}
+
+				var slotRange SlotRange
+				if strings.Contains(stringSlotRange, "-") {
+					slots := strings.Split(stringSlotRange, "-")
+					start, _ := strconv.Atoi(slots[0])
+					end, _ := strconv.Atoi(slots[1])
+					slotRange = SlotRange{Start: start, End: end}
+				} else {
+					slot, _ := strconv.Atoi(stringSlotRange)
+					slotRange = SlotRange{Start: slot, End: slot}
+				}
+				clusterNode.SlotRanges = append(clusterNode.SlotRanges, slotRange)
+			}
+
+			topology.Masters = append(topology.Masters, clusterNode)
+		} else if clusterNode.Role == NodeRoleSlave {
+			topology.Replicas = append(topology.Replicas, clusterNode)
+		}
+
+		topology.Nodes[clusterNode.ID] = clusterNode
+	}
+
+	return topology, nil
 }
