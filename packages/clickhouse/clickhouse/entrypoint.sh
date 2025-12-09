bc8aaa2fe3f07237c29e31d4cf3c742f3f082d58
Jason Kwok <35944715+Pandoks@users.noreply.github.com>
2025-12-09 15:27:25 -0800

    feat(clickhouse): Scalable clickhouse cluster (#5)
    * update(clickhouse): init
    
    * update(clickhouse): users.yaml
    
    * update(clickhouse): clickhouse server dockerfile
    
    * update(chart): services for clickhouse server
    
    * update(kube): kustomization scaffold with configmaps
    
    * update(clickhouse): initial ch config
    
    * update(clickhouse): entrypoint fills in rest of config
    
    * update(gh): added clickhouse to building actions
    
    * update(clickhouse): clickhouse keeper dockerfile
    
    * update(clickhouse): keeper config init
    
    * update(clickhouse): keeper entrypoint fills in missing config
    
    * update(clickhouse): clickhouse server statefulset init
    
    * update(clickhouse): mount yaml configs to server
    
    * update(clickhouse): data and logs pvc definitions
    
    * update(clickhouse): helm chart definition
    
    * update(clickhouse): headless service for keeper
    
    * update(clickhouse): check replicas is odd and >= 3
    
    * update(clickhouse): keeper config volume config map
    
    * update(clickhouse): clickhouse inidividual resources
    
    * update(clickhouse): resources keeper definition
    
    * update(clickhouse): volume claim templates
    
    * fix(clickhouse): set group and user for chown
    
    * update(clickhouse): keeper checks for all needed envs
    
    * update(clickhouse): helm values init
    
    * update(clickhouse): init main helmchart
    
    * update(clickhouse): local development dev patch
    
    * update(clickhouse): initial backup script
    
    * update(clickhouse): get base backup clause for diff/incr backups
    
    * update(clickhouse): do the backup
    
    * update(backup): dockerfile init
    
    * update(backup): build and push package.json scripts
    
    * update(backup): move backup script to docker dir
    
    * update(backup): formatting and use echo
    
    * update(backup): proper s3 links
    
    * update(backup): backup start end logs
    
    * update(backup): get s3 backup entries
    
    * update(backup): more success logging
    
    * update(backup): aws creds guard
    
    * fix(backup): typo schema->scheme
    
    * update(backup): actually remove the file from s3
    
    * update(backup): default values for chart
    
    * update(backup): local backup image
    
    * update(backup): build backup image in gh actions
    
    * update(backup): guard checks for all env needed
    
    * update(backup): use example creds
    
    * update(backup): init backup cronjob template
    
    * update(backup): rclone instead of aws
    
    * update(backup): full env vars
    
    * update(backup): add s3 region
    
    * fix(backup): space style
    
    * update(backup): better host nameing
    
    * update(backup): rclone s3 options
    
    * update(backup): entries using rclone
    
    * update(backup): delete backup using rclone
    
    * fix(backup): retention typo
    
    * fix(backup): use context from template definition
    
    * update: secrets set
    
    * update: secrets in manager
    
    * update(kube): add clickhouse to cluster kustomization
    
    * update(kube): add clickhouse dev patch
    
    * update(template): use global scope in loop
    
    * fix(clickhouse): proper config rendering
    
    * update(keeper): readiness probe
    
    * update(clickhouse): decrase local cluster resource usage
    
    * fix(backup): no more password encrypted backups
    
    password encrypted backups only work with backing up to disk. you need
    to setup your own cloud encryption from the cloud provider
    
    * update(backup): no backup password imported
    
    * update(backup): exclude certain databases that can't be backed up
    
    * update(backup): no need for tls option
    
    * update(backup): rclone cmd helper function
    
    * update(backup): use rclone helper

---
+++ packages/clickhouse/clickhouse/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+set -eu
+
+for v in \
+  NAMESPACE \
+  CLUSTER_NAME \
+  SHARD_ID \
+  REPLICA_ID \
+  KEEPER_REPLICAS \
+  SHARDS \
+  REPLICAS_PER_SHARD; do
+  eval ": \${$v:?Missing $v}"
+done
+
+envsubst </tmp/conf_templates/config.yaml >/etc/clickhouse-server/config.yaml
+envsubst </tmp/conf_templates/users.yaml >/etc/clickhouse-server/users.yaml
+
+cat >> /etc/clickhouse-server/config.yaml <<EOF
+
+zookeeper:
+  nodes:
+EOF
+for i in $(seq 0 $((KEEPER_REPLICAS - 1))); do
+  cat >> /etc/clickhouse-server/config.yaml <<EOF
+    - host: clickhouse-keeper-$CLUSTER_NAME-$i.clickhouse-keeper-$CLUSTER_NAME-headless.$NAMESPACE.svc.cluster.local
+      port: 9181
+EOF
+done
+
+cat >> /etc/clickhouse-server/config.yaml <<EOF
+
+remote_servers:
+  $CLUSTER_NAME:
+    shards:
+EOF
+for i in $(seq 0 $((SHARDS - 1))); do
+  echo "      replicas:" >> /etc/clickhouse-server/config.yaml
+  for j in $(seq 0 $((REPLICAS_PER_SHARD - 1))); do
+    cat >> /etc/clickhouse-server/config.yaml <<EOF
+          - host: clickhouse-$CLUSTER_NAME-shard-$i-$j.clickhouse-$CLUSTER_NAME-headless.$NAMESPACE.svc.cluster.local
+            port: 9000
+EOF
+  done
+done
+
+exec "$@"
