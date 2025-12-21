0173c39999b4081597d4f6e368eb5d49d53b2d83
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-12-21 13:09:53 -0800

    update(secrets): main function in js

--- scripts/lib/secrets.js
+++ scripts/lib/secrets.js
@@ -1,10 +1,14 @@
 import { Resource } from 'sst';
 
-const resourceKeys = Object.keys(Resource);
-const jsonResource = {};
-for (const key of resourceKeys) {
-  const resource = Resource[key];
-  jsonResource[key] = resource;
+function main() {
+  const resourceKeys = Object.keys(Resource);
+  const jsonResource = {};
+  for (const key of resourceKeys) {
+    const resource = Resource[key];
+    jsonResource[key] = resource;
+  }
+
+  console.log(JSON.stringify(jsonResource));
 }
 
-console.log(JSON.stringify(jsonResource));
+main();
