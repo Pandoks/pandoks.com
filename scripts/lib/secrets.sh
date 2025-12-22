290143c90720a827ddf27612adf951ad8bfe5c45
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-12-21 21:19:17 -0800

    update(secrets): format secrets to key value only
    needs to be parsed via jq

--- scripts/lib/secrets.sh
+++ scripts/lib/secrets.sh
@@ -3,6 +3,13 @@
 get_sst_secrets() {
   # NOTE: sst shell automatically sets the directory to the project root unless you are in a
   # subdirectory that is a pnpm workspace.
-  get_sst_secrets_json="$(pnpm sst shell node scripts/lib/secrets.js)"
+  get_sst_secrets_json="$(
+    pnpm sst shell node scripts/lib/sst-resources.js \
+      | jq 'to_entries
+            | map(select(.value.type == "sst.sst.Secret"))
+            | map({key: .key, value: .value.value})
+            | from_entries'
+  )"
+
   printf '%s\n' "${get_sst_secrets_json}"
 }
