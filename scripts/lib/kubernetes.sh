7f7c5f183ffc6191d5f7bce60f2790d2e7a817e0
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-12-22 20:18:35 -0800

    update(kubernetes): validate_and_get_absolute_kubeconfig_path

--- scripts/lib/kubernetes.sh
+++ scripts/lib/kubernetes.sh
@@ -25,3 +25,29 @@ wait_for_crd() {
     wait_for_crd_timeout=$((wait_for_crd_timeout - 2))
   done
 }
+
+#######################################
+# Validate kubeconfig file and return absolute path.
+# Arguments:
+#   Path to kubeconfig file
+# Outputs:
+#   Absolute path to kubeconfig file to stdout
+# Returns:
+#   0 on success, exits with 1 on failure
+#######################################
+validate_and_get_absolute_kubeconfig_path() {
+  validate_and_get_absolute_kubeconfig_path_file="$1"
+
+  if [ ! -f "${validate_and_get_absolute_kubeconfig_path_file}" ]; then
+    if [ -n "${RED:-}" ]; then
+      printf "%bError:%b kubeconfig not found: %s\n" "${RED}" "${NORMAL}" "${validate_and_get_absolute_kubeconfig_path_file}" >&2
+    else
+      echo "Error: kubeconfig not found: ${validate_and_get_absolute_kubeconfig_path_file}" >&2
+    fi
+    exit 1
+  fi
+
+  validate_and_get_absolute_kubeconfig_path_absolute_dir="$(cd "$(dirname "${validate_and_get_absolute_kubeconfig_path_file}")" && pwd)"
+  validate_and_get_absolute_kubeconfig_path_base_name="$(basename "${validate_and_get_absolute_kubeconfig_path_file}")"
+  printf "%s" "${validate_and_get_absolute_kubeconfig_path_absolute_dir}/${validate_and_get_absolute_kubeconfig_path_base_name}"
+}
