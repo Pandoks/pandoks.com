04705ea307e64d35fa87ed66a8f16f9ef516e9c9
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-03 22:26:31 -0800

    update(sst): cloudflare version

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,7 @@ export default $config({
         aws: {
           profile: process.env.GITHUB_ACTIONS ? undefined : 'Personal'
         },
-        cloudflare: '6.10.0',
+        cloudflare: '6.12.0',
         github: '6.7.2',
         hcloud: { token: process.env.HCLOUD_TOKEN, version: '1.24.0' }
       }
