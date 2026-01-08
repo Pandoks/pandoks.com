fdfb041a4986232096c6f7f8164bb3082c073b8f
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-07 16:37:31 -0800

    update(sst): local command pulumi provider

--- sst.config.ts
+++ sst.config.ts
@@ -13,7 +13,8 @@ export default $config({
         cloudflare: '6.12.0',
         github: '6.7.2',
         hcloud: { token: process.env.HCLOUD_TOKEN, version: '1.24.0' },
-        tailscale: { apiKey: process.env.TAILSCALE_API_KEY, version: '0.24.0' }
+        tailscale: { apiKey: process.env.TAILSCALE_API_KEY, version: '0.24.0' },
+        command: '1.1.3'
       }
     };
   },
