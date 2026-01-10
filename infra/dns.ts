f154940b9ad659e2749326bb8ac257d462d25f40
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-09 23:45:38 -0800

    update(dns): include helper variables

--- infra/dns.ts
+++ infra/dns.ts
@@ -1,5 +1,7 @@
-export const domain = $app.stage === 'production' ? 'pandoks.com' : 'dev.pandoks.com';
+export const isProduction = $app.stage === 'production';
+
+export const domain = isProduction ? 'pandoks.com' : 'dev.pandoks.com';
 
 export const EXAMPLE_DOMAIN = 'example.pandoks.com';
 
-export const STAGE_NAME = $app.stage === 'production' ? 'prod' : 'dev';
+export const STAGE_NAME = isProduction ? 'prod' : 'dev';
