270dffc641a10037ab887a4b33341b65b55e2945
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-13 11:51:45 -0800

    update(sst): set stage name as secret

--- infra/dns.ts
+++ infra/dns.ts
@@ -1,3 +1,5 @@
+import { secrets, setSecret } from './secrets';
+
 export const isProduction = $app.stage === 'production';
 
 export const domain = isProduction ? 'pandoks.com' : 'dev.pandoks.com';
@@ -5,3 +7,5 @@ export const domain = isProduction ? 'pandoks.com' : 'dev.pandoks.com';
 export const EXAMPLE_DOMAIN = 'example.pandoks.com';
 
 export const STAGE_NAME = isProduction ? 'prod' : 'dev';
+
+setSecret(secrets.Stage.name, STAGE_NAME);
