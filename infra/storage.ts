4679acc79cba19fe88e1d6617fd53896f6a19e22
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-15 10:54:12 -0800

    update(storage): endpoint uses new var

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,12 +1,11 @@
-import { STAGE_NAME } from './dns';
-import { secrets } from './secrets';
+import { STAGE_NAME, cloudflareAccountId } from './dns';
 
 export const backupBucket = new cloudflare.R2Bucket('BackupBucket', {
   name: `${STAGE_NAME}-backups`,
-  accountId: secrets.cloudflare.AccountId.value,
+  accountId: cloudflareAccountId,
   jurisdiction: 'default',
   location: 'wnam',
   storageClass: 'Standard'
 });
 
-export const s3Endpoint = $interpolate`${secrets.cloudflare.AccountId.value}.r2.cloudflarestorage.com`;
+export const s3Endpoint = `${cloudflareAccountId}.r2.cloudflarestorage.com`;
