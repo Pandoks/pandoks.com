c74ed234d44a967a286a048cfc59faa5c8cd06e4
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-19 09:26:15 -0800

    update(scripts): sst resource return flat

--- scripts/lib/sst-resources.js
+++ scripts/lib/sst-resources.js
@@ -2,13 +2,16 @@ import { Resource } from 'sst';
 
 function main() {
   const resourceKeys = Object.keys(Resource);
-  const jsonResource = {};
+  const flatResource = {};
   for (const key of resourceKeys) {
     const resource = Resource[key];
-    jsonResource[key] = resource;
+    const value = resource.value ?? resource.name ?? null;
+    if (value != null) {
+      flatResource[key] = value;
+    }
   }
 
-  console.log(JSON.stringify(jsonResource));
+  console.log(JSON.stringify(flatResource));
 }
 
 main();
