80478428ffc1250e0cc4d6e856e8be99018829eb
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-26 00:06:28 -0800

    update(vps): no production vps cluster yet

--- infra/vps/vps.ts
+++ infra/vps/vps.ts
@@ -6,7 +6,7 @@ import { createLoadBalancers } from './load-balancers';
 import { deleteTailscaleDevices } from '../tailscale';
 
 // NOTE: if you want to downsize the cluster, remember to manually drain remove the nodes with `kubectl drain` & `kubectl delete node`
-const CONTROL_PLANE_NODE_COUNT = isProduction ? 3 : 0;
+const CONTROL_PLANE_NODE_COUNT = isProduction ? 0 : 0;
 const CONTROL_PLANE_HOST_START_OCTET = 10; // starts at 10.0.1.<CONTROL_PLANE_HOST_START_OCTET>
 const WORKER_NODE_COUNT = isProduction ? 0 : 0;
 const WORKER_HOST_START_OCTET = 20; // starts at 10.0.1.<WORKER_HOST_START_OCTET> 20 allows for 10 control plane nodes
