566bc0c6719f7a78428ea5aa52cb00b59f69c7f3
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-01-28 16:39:26 -0800

    update(mobile): tuist project specification

---
+++ apps/mobile-template/ios/Project.swift
@@ -0,0 +1,69 @@
+import ProjectDescription
+
+let project = Project(
+    name: "MobileTemplate",
+    organizationName: "Pandoks",
+    targets: [
+        .target(
+            name: "MobileTemplate",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.pandoks.MobileTemplate",
+            infoPlist: .extendingDefault(with: [
+                "UILaunchScreen": [
+                    "UIColorName": "",
+                    "UIImageName": "",
+                ],
+            ]),
+            buildableFolders: [
+                "MobileTemplate/Sources",
+                "MobileTemplate/Resources",
+            ],
+            dependencies: [
+                .external(name: "PandoksSwift"),
+            ]
+        ),
+        .target(
+            name: "MobileTemplateTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "com.pandoks.MobileTemplateTests",
+            infoPlist: .default,
+            buildableFolders: [
+                "MobileTemplate/Tests",
+            ],
+            dependencies: [
+                .target(name: "MobileTemplate"),
+            ]
+        ),
+        .target(
+            name: "MobileTemplateWatch",
+            destinations: [.appleWatch],
+            product: .app,
+            bundleId: "com.pandoks.MobileTemplate.watchkitapp",
+            infoPlist: .extendingDefault(with: [
+                "WKApplication": true,
+            ]),
+            buildableFolders: [
+                "MobileTemplateWatch/Sources",
+                "MobileTemplateWatch/Resources",
+            ],
+            dependencies: [
+                .external(name: "PandoksSwift"),
+            ]
+        ),
+        .target(
+            name: "MobileTemplateWatchTests",
+            destinations: [.appleWatch],
+            product: .unitTests,
+            bundleId: "com.pandoks.MobileTemplateWatchTests",
+            infoPlist: .default,
+            buildableFolders: [
+                "MobileTemplateWatch/Tests",
+            ],
+            dependencies: [
+                .target(name: "MobileTemplateWatch"),
+            ]
+        ),
+    ]
+)
