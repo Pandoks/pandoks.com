809adf4e474c0157f34d15ab71674bb865bd2e0f
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-04-14 13:39:10 -0700

    update(web): better cache policies

--- infra/website.ts
+++ infra/website.ts
@@ -11,5 +11,29 @@ export const staticWebsite = new sst.cloudflare.StaticSite('StaticWebsite', {
   environment: {
     NOTION_API_KEY: secrets.notion.ApiKey.value,
     BLOG_NOTION_DATABASE_ID: '20f1bb259e4b804ba24be1ceebf4c761'
+  },
+  assets: {
+    fileOptions: [
+      {
+        files: '**',
+        cacheControl: 'max-age=0,no-cache,no-store,must-revalidate'
+      },
+      {
+        files: ['**/*.js', '**/*.css'],
+        cacheControl: 'max-age=31536000,public,immutable'
+      },
+      {
+        files: 'fonts/**',
+        cacheControl: 'max-age=31536000,public,immutable'
+      },
+      {
+        files: 'favicon/**',
+        cacheControl: 'max-age=31536000,public,immutable'
+      },
+      {
+        files: '**/__data.json',
+        cacheControl: 'max-age=3600,public'
+      }
+    ]
   }
 });
