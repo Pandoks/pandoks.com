aebcbe4090211bb725d488e03b9ec3e04b3636de
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-05-25 04:34:07 -0700

    update(infra): imagebuilder roles and profiles needed

--- infra/builder/ami.ts
+++ infra/builder/ami.ts
@@ -8,3 +8,22 @@ const VERSION = '1.0.0';
 const PARENT_IMAGE_ARN_X86 = `arn:aws:imagebuilder:${awsRegion}:aws:image/ubuntu-server-24-lts-x86/x.x.x`;
 const PARENT_IMAGE_ARN_ARM64 = `arn:aws:imagebuilder:${awsRegion}:aws:image/ubuntu-server-24-lts-arm64/x.x.x`;
 
+const bakeInstanceRole = new aws.iam.Role('BuilderBakeInstanceRole', {
+  assumeRolePolicy: aws.iam.getPolicyDocumentOutput({
+    statements: [
+      {
+        effect: 'Allow',
+        principals: [{ type: 'Service', identifiers: ['ec2.amazonaws.com'] }],
+        actions: ['sts:AssumeRole']
+      }
+    ]
+  }).json,
+  managedPolicyArns: [
+    'arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore',
+    'arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilder'
+  ]
+});
+const bakeInstanceProfile = new aws.iam.InstanceProfile('BuilderBakeInstanceProfile', {
+  role: bakeInstanceRole.name
+});
+
