2891c755bf72b590ffd05fed6256f2af410b82d4
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-05-25 04:35:30 -0700

    update(infra): imagebuilder configs recipes and components

--- infra/builder/ami.ts
+++ infra/builder/ami.ts
@@ -5,9 +5,6 @@ import { awsRegion, STAGE_NAME } from '../dns';
 // WARNING: version must be bumped when AMI changes to rebuild
 const VERSION = '1.0.0';
 
-const PARENT_IMAGE_ARN_X86 = `arn:aws:imagebuilder:${awsRegion}:aws:image/ubuntu-server-24-lts-x86/x.x.x`;
-const PARENT_IMAGE_ARN_ARM64 = `arn:aws:imagebuilder:${awsRegion}:aws:image/ubuntu-server-24-lts-arm64/x.x.x`;
-
 const bakeInstanceRole = new aws.iam.Role('BuilderBakeInstanceRole', {
   assumeRolePolicy: aws.iam.getPolicyDocumentOutput({
     statements: [
@@ -27,3 +24,40 @@ const bakeInstanceProfile = new aws.iam.InstanceProfile('BuilderBakeInstanceProf
   role: bakeInstanceRole.name
 });
 
+const builderToolsComponent = new aws.imagebuilder.Component('BuilderToolsComponent', {
+  name: `builder-tools`,
+  platform: 'Linux',
+  version: VERSION,
+  data: readFileSync(join(process.cwd(), 'infra/builder/ami.yaml'), 'utf-8')
+});
+const builderRecipeX86 = new aws.imagebuilder.ImageRecipe('BuilderRecipeX86', {
+  name: `builder-x86`,
+  parentImage: `arn:aws:imagebuilder:${awsRegion}:aws:image/ubuntu-server-24-lts-x86/x.x.x`,
+  version: VERSION,
+  components: [{ componentArn: builderToolsComponent.arn }]
+});
+const builderRecipeArm64 = new aws.imagebuilder.ImageRecipe('BuilderRecipeArm64', {
+  name: `builder-arm64`,
+  parentImage: `arn:aws:imagebuilder:${awsRegion}:aws:image/ubuntu-server-24-lts-arm64/x.x.x`,
+  version: VERSION,
+  components: [{ componentArn: builderToolsComponent.arn }]
+});
+
+const builderBakeInfraX86 = new aws.imagebuilder.InfrastructureConfiguration(
+  'BuilderBakeInfraX86',
+  {
+    name: `builder-bake-x86`,
+    instanceTypes: ['c7i.large'],
+    instanceProfileName: bakeInstanceProfile.name,
+    terminateInstanceOnFailure: true
+  }
+);
+const builderBakeInfraArm64 = new aws.imagebuilder.InfrastructureConfiguration(
+  'BuilderBakeInfraArm64',
+  {
+    name: `builder-bake-arm64`,
+    instanceTypes: ['c7g.large'],
+    instanceProfileName: bakeInstanceProfile.name,
+    terminateInstanceOnFailure: true
+  }
+);
