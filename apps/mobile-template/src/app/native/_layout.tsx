f0a0c65391a25b8accce92aef37f2597a14a0d8f
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-06-14 22:51:42 -0700

    update(mobile): demos layout -> native layout

--- apps/mobile-template/src/app/native/_layout.tsx
+++ apps/mobile-template/src/app/native/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from 'expo-router';
 
-export default function DemosLayout() {
-  return <Stack screenOptions={{ headerBackTitle: 'Back' }} />;
+export default function NativeLayout() {
+  return (
+    <Stack screenOptions={{ headerBackTitle: 'Back' }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
