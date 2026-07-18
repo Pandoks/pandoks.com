3a3530dfcb7dd08cac432009ff2f6465e6261ccc
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-07-18 02:16:31 -0700

    refactor(infra): stop managing the dev VPS

--- infra/dev.ts
+++ infra/dev.ts
@@ -1,9 +1,3 @@
-import { readFileSync } from 'node:fs';
-import { getFlavorId, getImageId, OVH_CLOUD_PROJECT_SERVICE } from './ovh';
-import { tailscaleAcl } from './tailscale';
-import { renderCloudInit } from './utils';
-import { deleteServerFromTailnet } from './vps/servers';
-
 new sst.x.DevCommand('DevInit', {
   dev: {
     title: 'InitDev',
@@ -35,50 +29,5 @@ new sst.x.DevCommand('K3dDependencyRestart', {
     autostart: false
   }
 });
-
-if ($app.stage === 'pandoks') {
-  const tailscaleHostname = `${$app.stage}-dev-box`;
-  const registrationTailnetAuthKey = new tailscale.TailnetKey(
-    'OvhDevBoxTailnetRegistrationAuthKey',
-    {
-      description: `ovh ${$app.stage} dev box registration`,
-      reusable: false,
-      expiry: 1800,
-      preauthorized: true,
-      tags: ['tag:ovh', 'tag:dev']
-    },
-    { dependsOn: [tailscaleAcl] }
-  );
-
-  const cloudInitConfig = readFileSync(`${process.cwd()}/infra/dev-cloud-config.yaml`, 'utf8');
-  const userData = registrationTailnetAuthKey.key.apply((key) => {
-    const environment = {
-      REGISTRATION_TAILNET_AUTH_KEY: key,
-      TAILSCALE_HOSTNAME: tailscaleHostname
-    };
-
-    return renderCloudInit(cloudInitConfig, environment);
-  });
-
-  const devBoxRegion = 'US-WEST-OR-1';
-  new ovh.cloudproject.Instance(
-    'OvhDevBox',
-    {
-      serviceName: OVH_CLOUD_PROJECT_SERVICE,
-      name: tailscaleHostname,
-      region: devBoxRegion,
-      billingPeriod: 'hourly',
-      flavor: { flavorId: await getFlavorId(devBoxRegion, 'd2-4') },
-      bootFrom: { imageId: await getImageId(devBoxRegion, 'Ubuntu 24.04') },
-      network: { public: true },
-      userData
-    },
-    {
-      hooks: {
-        afterDelete: [deleteServerFromTailnet]
-      }
-    }
-  );
-}
 
 export {};
