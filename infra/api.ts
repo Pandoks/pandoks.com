42d58076570dcc12113ccef5d706a2e495e2add8
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2026-08-13 07:06:21 +0000

    update: revert

--- infra/api.ts
+++ infra/api.ts
@@ -1,5 +1,5 @@
 import { secrets } from './secrets';
-import { createCloudflareDns, domain, isProduction } from './dns';
+import { domain, isProduction } from './dns';
 import { githubOrg, githubRepoName } from './github';
 
 const apiDomain = `api.${domain}`;
@@ -13,7 +13,7 @@ const notion = new sst.Linkable('Notion', {
 export const apiRouter = new sst.aws.Router('ApiRouter', {
   domain: {
     name: apiDomain,
-    dns: createCloudflareDns()
+    dns: sst.cloudflare.dns()
   }
 });
 
